1f0dfec611b3b2f30ba31b02acf40abacac071bb
Seunghyo <shyo0218@naver.com>
2023-06-12 11:39:18 +0900

    템플릿 수정

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+
+const ChangeVauleButton = styled.button`
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  background-color: beige;
+  font-size: 36px;
+  border: 0;
+  border-radius: 50%;
+  width: 4rem;
+  height: 4rem;
+  height: 4rem;
+  margin: 1rem;
+`;
+
+const ButtonWrapper = styled.div`
+  margin: 1rem;
+`;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ButtonWrapper>
+          <ChangeVauleButton>+</ChangeVauleButton>
+          <ChangeVauleButton>-</ChangeVauleButton>
+        </ButtonWrapper>
       </header>
     </div>
   );
